55cd91ca1a77597e346598086e85cc1fcff2eebd
betaEncoder <betaencoder@gmail.com>
2020-05-15 00:39:43 +0900

    first release

---
+++ Now_ON_AIR/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Now_ON_AIR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label_status = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_key = new System.Windows.Forms.TextBox();
+            this.textBox_on_event = new System.Windows.Forms.TextBox();
+            this.textBox_off_event = new System.Windows.Forms.TextBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(13, 13);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(44, 12);
+            this.label_status.TabIndex = 0;
+            this.label_status.Text = "Status: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ON event name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "OFF event name";
+            // 
+            // textBox_key
+            // 
+            this.textBox_key.Location = new System.Drawing.Point(109, 35);
+            this.textBox_key.Name = "textBox_key";
+            this.textBox_key.Size = new System.Drawing.Size(250, 19);
+            this.textBox_key.TabIndex = 4;
+            // 
+            // textBox_on_event
+            // 
+            this.textBox_on_event.Location = new System.Drawing.Point(109, 60);
+            this.textBox_on_event.Name = "textBox_on_event";
+            this.textBox_on_event.Size = new System.Drawing.Size(100, 19);
+            this.textBox_on_event.TabIndex = 5;
+            // 
+            // textBox_off_event
+            // 
+            this.textBox_off_event.Location = new System.Drawing.Point(109, 85);
+            this.textBox_off_event.Name = "textBox_off_event";
+            this.textBox_off_event.Size = new System.Drawing.Size(100, 19);
+            this.textBox_off_event.TabIndex = 6;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Now ON AIR";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 113);
+            this.Controls.Add(this.textBox_off_event);
+            this.Controls.Add(this.textBox_on_event);
+            this.Controls.Add(this.textBox_key);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label_status);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Now ON AIR";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ClientSizeChanged += new System.EventHandler(this.Form1_ClientSizeChanged);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label_status;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_key;
+        private System.Windows.Forms.TextBox textBox_on_event;
+        private System.Windows.Forms.TextBox textBox_off_event;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Now_ON_AIR/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace Now_ON_AIR
+{
+    public partial class Form1 : Form
+    {
+        bool on_air = false;
+        Icon icon_OA;
+        Icon icon_nOA;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool current_state = false;
+            // triggered 1sec period
+
+            System.Diagnostics.Process[] ps = System.Diagnostics.Process.GetProcesses();    // get all of processes
+            foreach (System.Diagnostics.Process p in ps)
+            {
+                try
+                {
+                    if (p.ProcessName.Contains("CptHost"))
+                    {
+                        Console.WriteLine("ID:{0}", p.Id);
+                        current_state = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("エラー: {0}", ex.Message);
+                }
+            }
+            if (current_state == true)
+            {
+                this.label_status.Text = "Status: ON AIR";
+            }
+            else
+            {
+                this.label_status.Text = "Status: OFF_AIR";
+            }
+
+            var client = new HttpClient();
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                /** POSTするデータなし **/
+            });
+            string ifttt_URL = "https://maker.ifttt.com/trigger/";
+            string key = this.textBox_key.Text;
+            string ON_event = this.textBox_on_event.Text;
+            string OFF_event = this.textBox_off_event.Text;
+            if (key == "" || ON_event == "" || OFF_event == "")
+            {
+                return;
+            }
+            if (on_air == false && current_state == true)
+            {
+                // turn on trigger
+                client.PostAsync(ifttt_URL + ON_event + "/with/key/" + key, content);
+                this.Icon = icon_OA;
+                notifyIcon1.Icon = icon_OA;
+            }
+            else if (on_air == true && current_state == false)
+            {
+                // turn off trigger
+                client.PostAsync(ifttt_URL + OFF_event + "/with/key/" + key, content);
+                this.Icon = icon_nOA;
+                notifyIcon1.Icon = icon_nOA;
+            }
+            on_air = current_state;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.saved_key = this.textBox_key.Text;
+            Properties.Settings.Default.saved_on_event = this.textBox_on_event.Text;
+            Properties.Settings.Default.saved_off_event = this.textBox_off_event.Text;
+            Properties.Settings.Default.Save();
+            notifyIcon1.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox_key.Text = Properties.Settings.Default.saved_key;
+            textBox_on_event.Text = Properties.Settings.Default.saved_on_event;
+            textBox_off_event.Text = Properties.Settings.Default.saved_off_event;
+            //現在のコードを実行しているAssemblyを取得
+            System.Reflection.Assembly myAssembly = System.Reflection.Assembly.GetExecutingAssembly();
+            icon_OA = new Icon(myAssembly.GetManifestResourceStream("Now_ON_AIR.Resources.OA.ico"));
+            icon_nOA = new Icon(myAssembly.GetManifestResourceStream("Now_ON_AIR.Resources.nOA.ico"));
+        }
+
+        private void Form1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == System.Windows.Forms.FormWindowState.Minimized)
+            {                // フォームが最小化の状態であればフォームを非表示にする
+                this.Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true;
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            this.Activate();
+        }
+
+    }
+}
